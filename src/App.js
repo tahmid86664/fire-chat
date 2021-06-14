a071a71d74c83d64915dc0bd567f5558576df6e1
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2021-06-14 15:29:55 +0600

    added firebase auth and chatengine

--- src/App.js
+++ src/App.js
@@ -7,15 +7,22 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+// context
+import { AuthProvider } from './context/AuthContext';
+
 import Login from './components/login/login';
+import Chats from './components/chats/chats';
 
 function App() {
   return (
     <div className="app">
       <Router>
-        <Switch>
-          <Route path='/' component={Login} />
-        </Switch>
+        <AuthProvider>
+          <Switch>
+            <Route path='/chats' component={Chats} />
+            <Route path='/' component={Login} />
+          </Switch>
+        </AuthProvider>
       </Router>
     </div>
   );
---
+++ src/chat-engine.js
@@ -0,0 +1,4 @@
+// export const projectId = "9988060d-06d6-44bd-ba47-b32613c63e88"; // from chatengine.io by creating a project
+export const alreadyUserUrl = "https://api.chatengine.io/users/me";
+export const createUserUrl = "https://api.chatengine.io/users";
+// export const privateKey = "de7efa56-edc1-484e-8a69-1c7e6f83cb6e";
